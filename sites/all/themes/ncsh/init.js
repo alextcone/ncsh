09fcfe21f81d442eb2c05e8e8633ab350db4f071
Alex <alextcone@gmail.com>
2012-07-08 18:26:02 -0400

    css for site-wide header area

---
+++ sites/all/themes/ncsh/init.js
@@ -0,0 +1,23 @@
+jQuery(function( $ ){
+	// The default axis is 'y', but in this demo, I want to scroll both
+	// You can modify any default like this
+	$.localScroll.defaults.axis = 'xy';
+	
+	// Scroll initially if there's a hash (#something) in the url 
+	$.localScroll.hash({
+		queue:true,
+		duration:1500
+	});
+	
+	$.localScroll({
+		queue:true,
+		duration:1000,
+		hash:true,
+		onBefore:function( e, anchor, $target ){
+			// The 'this' is the settings object, can be modified
+		},
+		onAfter:function( anchor, settings ){
+			// The 'this' contains the scrolled element (#content)
+		}
+	});
+});
