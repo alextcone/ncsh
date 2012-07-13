0908ba8b38f9cbeef96e659471bc832a7f510ec4
Alex <alextcone@gmail.com>
2012-07-13 00:18:39 -0400

    homepage css and layout plus about page javascript

--- sites/all/themes/ncsh/init.js
+++ sites/all/themes/ncsh/init.js
@@ -1,29 +1,4 @@
-jQuery(function( $ ){
-	// The default axis is 'y', but in this demo, I want to scroll both
-	// You can modify any default like this
-	$.localScroll.defaults.axis = 'xy';
-	
-	// Scroll initially if there's a hash (#something) in the url 
-	$.localScroll.hash({
-		queue:true,
-		duration:1500
-	});
-	
-	$.localScroll({
-		queue:true,
-		duration:1000,
-		hash:true,
-		onBefore:function( e, anchor, $target ){
-			// The 'this' is the settings object, can be modified
-		},
-		onAfter:function( anchor, settings ){
-			// The 'this' contains the scrolled element (#content)
-		}
-	});
-});
-
 jQuery(function( $ ) {
-  // Do our DOM lookups beforehand
 	var nav_container = $(".zone-header");
 	var nav = $(".zone-header-wrapper");
 	
@@ -32,23 +7,28 @@ jQuery(function( $ ) {
 
 	nav_container.waypoint({
 		handler: function(event, direction) {
-			
 			if (direction == 'down') {
-			
 				nav_container.css({ 'height':nav.outerHeight() });		
 				nav.stop().addClass("sticky").css("top",-nav.outerHeight()).animate({"top":top_spacing});
-				
 			} else {
-			
 				nav_container.css({ 'height':'auto' });
 				nav.stop().removeClass("sticky").css("top",nav.outerHeight()+waypoint_offset).animate({"top":""});
-				
 			}
-			
 		},
 		offset: function() {
 			return -nav.outerHeight()-waypoint_offset;
 		}
 	});
- 
+});
+jQuery(function( $ ){
+	$(".zone-header-wrapper").localScroll({
+		queue:true,
+		duration:1000,
+		hash:true,
+		offset: {'left': 0, 'top': -0.1*$(window).height()},
+		onBefore:function( e, anchor, $target ){
+		},
+		onAfter:function( anchor, settings ){
+		}
+	});
 });
