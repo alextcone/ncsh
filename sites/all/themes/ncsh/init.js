d37df2b2127bcc8afefc53c99d9be577955ce288
Alex <alextcone@gmail.com>
2012-07-13 17:11:48 -0400

    added google maps api v3 and minor css updates

--- sites/all/themes/ncsh/init.js
+++ sites/all/themes/ncsh/init.js
@@ -2,11 +2,20 @@ jQuery(function( $ ){
 	$(".zone-header-wrapper").localScroll({
 		queue:true,
 		duration:1000,
-		hash:true,
-		offset: {'left': 0, 'top': -10},
+		offset: {'left': 0, 'top': -80},
 		onBefore:function( e, anchor, $target ){
 		},
 		onAfter:function( anchor, settings ){
 		}
 	});
 });
+
+jQuery(function( $ ){
+	$.waypoints.settings.scrollThrottle = 30;
+	$('.zone-header-wrapper').waypoint(function(event, direction) {
+		$(this).toggleClass('sticky', direction === "down");
+		event.stopPropagation();
+	});
+});
+
+
